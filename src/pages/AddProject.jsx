8dd8409e53ddbdf863679de9dacd25e494d15986
sameeejsx <samueliy185@gmail.com>
2025-09-07 16:24:20 +0100

    updates

--- src/pages/AddProject.jsx
+++ src/pages/AddProject.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ProjectForm from '../components/ProjectForm';
+import { useProject } from '../contexts/ProjectContext';
 
 const AddProject = () => {
+  const { createProject, loading } = useProject();
+
+  const handleSubmit = async (formData) => {
+    await createProject(formData);
+  };
+
   return (
     <div className="pt-20">
-        <ProjectForm />
+      <ProjectForm onSubmit={handleSubmit} loading={loading.create} />
     </div>
-  )
-}
+  );
+};
 
-export default AddProject
+export default AddProject;
